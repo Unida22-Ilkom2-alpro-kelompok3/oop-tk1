d10934738fb45bcf471ef615ef57357ef76ec958
rizki-rianto666 <115854620+rizki-rianto666@users.noreply.github.com>
2023-04-13 23:08:10 +0700

    Add files via upload

--- forms.php
+++ forms.php
@@ -6,7 +6,53 @@
 <body>
 
 <h2>Form Submit</h2>
+<style>
+	h2{
+		font-family: Georgia, 'Times New Roman', Times, serif;
+		-webkit-text-stroke: 2px rgb(51, 182, 11);
+		color:rgb(33, 200, 4);
+		font-size:30px;
 
+	}
+	    .sub{
+        background-color: rgb(255, 255, 255);
+		padding-left: 20px;
+        padding-right: 20px;
+        padding-top: 6px;
+        padding-bottom: 6px;
+		font-weight: bold;
+        border-radius:5px;
+        cursor: pointer;
+		border: solid;
+		border-color: rgb(51, 182, 11);
+		color: rgb(51, 182, 11);
+		border-width:1px;
+		transition: all 0.15s;
+
+    }
+    .sub:hover{
+		color: rgb(255, 255, 255);
+		background-color: rgb(51, 182, 11);
+		
+    }
+
+	.sub:active{
+		opacity:0.8;
+	}
+
+	input{
+		background-color:rgb(131, 220, 104);
+		border-width:1px;
+		opacity:0.8;
+		border-radius: 3px;
+	}
+
+	label{
+		font-weight: 600;
+		color:rgb(51, 182, 11);
+
+	}
+</style>
 <form method="post" action="resubmit.php">
 	<label for="name">Name:</label>
 	<input type="text" name="name" id="name" placeholder="Username" required><br><br>
@@ -17,7 +63,7 @@
 	<label for="password">Password:</label>
 	<input type="password" name="password" id="password" placeholder="Password" required><br><br>
 
-	<input type="submit" name="submit" value="Submit">
+	<input class = "sub" type="submit" name="submit" value="Submit">
 </form>
 
 </body>
--- resubmit.php
+++ resubmit.php
@@ -4,21 +4,52 @@
 	<title>Resubmit</title>
 </head>
 <body>
+<style>
+	input{
+		background-color: rgb(255, 255, 255);
+		padding-left: 20px;
+        padding-right: 20px;
+        padding-top: 6px;
+        padding-bottom: 6px;
+		font-weight: bold;
+        border-radius: 5px;
+        cursor: pointer;
+		border: solid;
+		border-color: rgb(51, 182, 11);
+		color: rgb(51, 182, 11);
+		border-width:1px;
+		transition: all 0.15s;
+	}
+	input:hover{
+		color: rgb(255, 255, 255);
+		background-color: rgb(51, 182, 11);
+    }
 
+	input:active{
+		opacity:0.8;
+	}
+
+	.pe{
+		font-size:20px;
+		color:rgb(51, 182, 11);
+		background-color:rgb(255,255,255);
+	}
+
+</style>
 <?php
 if (isset($_POST["submit"])) {
 	$name = $_POST["name"];
 	$email = $_POST["email"];
 	$password = $_POST["password"];
 	
-	echo "Berhasil Login <br>";
-	echo "Name: $name<br>";
-	echo "Email: $email<br>";
-	echo "Password : $password<br>";
+	echo "<p class='pe'> Berhasil Login !</p>";
+	echo "<p>Name: $name<br></p>";
+	echo "<p>Email: $email<br></p>";
+	echo "<p>Password : $password<br></p>";
 ?>
 	<br>
 	<form method="post" action="forms.php">
-			<input type="submit" name="resubmit" value="Resubmit">
+			<input type="submit" name="resubmit" value="Submit Ulang">
 	</form>
 <?php
 }
