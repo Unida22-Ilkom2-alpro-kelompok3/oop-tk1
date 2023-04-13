8919eb832f2dc1bd7a5fd7922e9cd7aa5fb2c8ab
rizki-rianto666 <115854620+rizki-rianto666@users.noreply.github.com>
2023-04-13 22:07:36 +0700

    Add files via upload

---
+++ resubmit.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Resubmit</title>
+</head>
+<body>
+
+<?php
+if (isset($_POST["submit"])) {
+	$name = $_POST["name"];
+	$email = $_POST["email"];
+	$password = $_POST["password"];
+	
+	echo "Berhasil Login <br>";
+	echo "Name: $name<br>";
+	echo "Email: $email<br>";
+	echo "Password : $password<br>";
+?>
+	<br>
+	<form method="post" action="forms.php">
+			<input type="submit" name="resubmit" value="Submit Another Form">
+	</form>
+<?php
+}
+?>
+
+</body>
+</html>
